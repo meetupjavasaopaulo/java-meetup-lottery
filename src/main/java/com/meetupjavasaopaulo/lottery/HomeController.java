2d49708999b3afa2b95723d22a742c8def781358
Fernando Boaglio <boaglio@gmail.com>
2018-05-17 02:35:56 -0300

    about page

--- src/main/java/com/meetupjavasaopaulo/lottery/HomeController.java
+++ src/main/java/com/meetupjavasaopaulo/lottery/HomeController.java
@@ -17,42 +17,47 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    @Value("classpath:meetup.xls")
-    private Resource res;
+	@Value("classpath:meetup.xls")
+	private Resource res;
 
-    @GetMapping("/")
-    public ModelAndView home() {
+	@GetMapping("/")
+	public ModelAndView home() {
 
-        List<String> guests = new ArrayList<>();
-        String winner = "";
-        int lottery = 0;
+		List<String> guests = new ArrayList<>();
+		String winner = "";
+		int lottery = 0;
 
-        System.out.println("All files:");
-        try (Stream<String> stream = Files.lines(Paths.get(res.getURI()))) {
+		System.out.println("All files:");
+		try (Stream<String> stream = Files.lines(Paths.get(res.getURI()))) {
 
-            guests = stream.collect(Collectors.toList());
+			guests = stream.collect(Collectors.toList());
 
-            // guests.forEach(System.out::println);
+			// guests.forEach(System.out::println);
 
-            System.out.println("Stream size: " + guests.size());
+			System.out.println("Stream size: " + guests.size());
 
-            lottery = RandomUtil.getRandomNumberInRange(0, guests.size());
-            winner = guests.get(lottery);
+			lottery = RandomUtil.getRandomNumberInRange(0, guests.size());
+			winner = guests.get(lottery);
 
-            System.out.println("Winner: " + winner);
+			System.out.println("Winner: " + winner);
 
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 
-        ModelAndView mv = new ModelAndView("home");
-        mv.addObject("guests", guests);
-        mv.addObject("size", guests.size());
-        mv.addObject("winner", winner);
-        mv.addObject("lottery", lottery);
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("guests", guests);
+		mv.addObject("size", guests.size());
+		mv.addObject("winner", winner);
+		mv.addObject("lottery", lottery);
 
-        return mv;
+		return mv;
 
-    }
+	}
+
+	@GetMapping("/about")
+	public String sobre() {
+		return "about";
+	}
 
 }
